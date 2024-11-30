81bdf06f0df70e47afe78924a3f9de5dde172132
Gokul <pgokulmca@gmail.com>
2024-11-30 10:39:51 +0530

    Initial commit

---
+++ src/main/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplication.java
@@ -0,0 +1,13 @@
+package org.gklsan.springboot.myjavalearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaLearningApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyJavaLearningApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gklsan.springboot.myjavalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaLearningApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
