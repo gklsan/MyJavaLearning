1e67a605944c952e9860799c91768252bfba7059
Gokul <pgokulmca@gmail.com>
2024-12-02 11:45:00 +0000

    spring-boot-starter-security added

--- src/main/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplication.java
+++ src/main/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplication.java
@@ -8,7 +8,6 @@ public class MyJavaLearningApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MyJavaLearningApplication.class, args);
-    System.out.println("Hello World!");
   }
 
 }
