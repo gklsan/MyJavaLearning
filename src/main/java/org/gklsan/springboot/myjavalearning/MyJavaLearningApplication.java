2ca0f5a4795a6b552a2e2db736f10b72edb9ec34
Gokul <pgokulmca@gmail.com>
2024-12-06 19:08:00 +0530

    MySQL and its dependencies got added along with commandlineRunner

--- src/main/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplication.java
+++ src/main/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplication.java
@@ -1,20 +1,29 @@
 package org.gklsan.springboot.myjavalearning;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-//@SpringBootApplication(
-//        scanBasePackages = {
-//                "org.gklsan.springboot.util",
-//                "org.gklsan.springboot.myjavalearning"
-//        }
-//)
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.gklsan.springboot.crud",
+                "org.gklsan.springboot.myjavalearning"
+        }
+)
 
-@SpringBootApplication
+//@SpringBootApplication
 public class MyJavaLearningApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MyJavaLearningApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(String[] args) {
+    return runner -> {
+      System.out.println("Hello World");
+    };
   }
 
 }
