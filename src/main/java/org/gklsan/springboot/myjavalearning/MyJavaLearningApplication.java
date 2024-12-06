53a5ab32ab6cade68b8988ed1e0cca66cf1f8412
Gokul <pgokulmca@gmail.com>
2024-12-06 19:33:00 +0530

    Student entity and DAO(Data Access Object) and its implementaion added

--- src/main/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplication.java
+++ src/main/java/org/gklsan/springboot/myjavalearning/MyJavaLearningApplication.java
@@ -1,5 +1,7 @@
 package org.gklsan.springboot.myjavalearning;
 
+import org.gklsan.springboot.myjavalearning.dao.StudentDAO;
+import org.gklsan.springboot.myjavalearning.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,10 +22,35 @@ public class MyJavaLearningApplication {
   }
 
   @Bean
-  public CommandLineRunner commandLineRunner(String[] args) {
+  public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
     return runner -> {
       System.out.println("Hello World");
+      createStudent(studentDAO);
+      createMultipleStudents(studentDAO);
     };
+  }
+
+  private void createMultipleStudents(StudentDAO studentDAO) {
+    System.out.println("Creating multiple students...");
+    Student tmpStudent1 = new Student("Gokul 1", "P", "gokul1@p.com");
+    Student tmpStudent2 = new Student("Gokul 2", "P", "gokul2@p.com");
+    Student tmpStudent3 = new Student("Gokul 3", "P", "gokul3@p.com");
+
+    System.out.println("Saving the students...");
+    studentDAO.save(tmpStudent1);
+    studentDAO.save(tmpStudent2);
+    studentDAO.save(tmpStudent3);
+  }
+
+
+  private void createStudent(StudentDAO studentDAO) {
+    System.out.println("Creating student...");
+    Student tmpStudent = new Student("John", "Doe", "test@test.com");
+
+    System.out.println("Saving the student...");
+    studentDAO.save(tmpStudent);
+
+    System.out.println("Saved student: " + tmpStudent.getId());
   }
 
 }
---
+++ src/main/java/org/gklsan/springboot/myjavalearning/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package org.gklsan.springboot.myjavalearning.dao;
+
+import org.gklsan.springboot.myjavalearning.entity.Student;
+
+public interface StudentDAO {
+  void save(Student theStudent);
+}
