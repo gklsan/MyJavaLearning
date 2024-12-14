90a0506e3f63172aba4f3474e4a96a8505a9cd1c
Gokul <pgokulmca@gmail.com>
2024-12-14 08:48:00 +0530

    Dynamic username and passwords from database

---
+++ scripts/user_and_authorites.sql
@@ -0,0 +1,63 @@
+-- SELECT * FROM student_tracker.student;
+-- Truncate student_tracker.student;
+
+
+-- CREATE DATABASE productdb;
+-- USE productdb;
+-- CREATE TABLE products (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     price DOUBLE NOT NULL
+-- );
+
+
+USE `student_tracker`;
+
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+                         `username` varchar(50) NOT NULL,
+                         `password` varchar(50) NOT NULL,
+                         `enabled` tinyint NOT NULL,
+                         PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users`
+VALUES
+    ('john','{noop}test123',1),
+    ('mary','{noop}test123',1),
+    ('joe','{noop}test123',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+                               `username` varchar(50) NOT NULL,
+                               `authority` varchar(50) NOT NULL,
+                               UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+                               CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities`
+VALUES
+    ('john','ROLE_STUDENT'),
+    ('mary','ROLE_STUDENT'),
+    ('mary','ROLE_TEACHER'),
+    ('joe','ROLE_STUDENT'),
+    ('joe','ROLE_TEACHER'),
+    ('joe','ROLE_PRINCIPAL');
--- src/main/java/org/gklsan/springboot/myjavalearning/security/StudentSecurityConfig.java
+++ src/main/java/org/gklsan/springboot/myjavalearning/security/StudentSecurityConfig.java
@@ -9,31 +9,21 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
 import java.net.http.HttpRequest;
 
 @Configuration
 public class StudentSecurityConfig {
 
   @Bean
-  public InMemoryUserDetailsManager uerDetailsManager() {
-
-    UserDetails john = User.builder()
-                           .username("john").password("{noop}12345")
-                           .roles("STUDENT")
-                           .build();
-    UserDetails mary = User.builder()
-                           .username("mary").password("{noop}12345")
-                           .roles("TEACHER", "STUDENT")
-                           .build();
-    UserDetails joe = User.builder()
-                          .username("joe").password("{noop}12345")
-                          .roles("TEACHER", "STUDENT", "PRINCIPAL")
-                          .build();
-
-    return new InMemoryUserDetailsManager(john, mary, joe);
+  public UserDetailsManager userDetailsManager(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource);
   }
+
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -56,4 +46,25 @@ public class StudentSecurityConfig {
     http.csrf(AbstractHttpConfigurer::disable);
     return http.build();
   }
+
+
+
+//  @Bean
+//  public InMemoryUserDetailsManager uerDetailsManager() {
+//
+//    UserDetails john = User.builder()
+//                           .username("john").password("{noop}12345")
+//                           .roles("STUDENT")
+//                           .build();
+//    UserDetails mary = User.builder()
+//                           .username("mary").password("{noop}12345")
+//                           .roles("TEACHER", "STUDENT")
+//                           .build();
+//    UserDetails joe = User.builder()
+//                          .username("joe").password("{noop}12345")
+//                          .roles("TEACHER", "STUDENT", "PRINCIPAL")
+//                          .build();
+//
+//    return new InMemoryUserDetailsManager(john, mary, joe);
+//  }
 }
